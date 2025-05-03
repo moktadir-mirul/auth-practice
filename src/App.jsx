773ea01070901be2edded9c4a4458913ba49d042
moktadir-mirul <moktadir.mirul@gmail.com>
2025-05-03 17:05:51 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-center text-3xl py-8'>I am home</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
